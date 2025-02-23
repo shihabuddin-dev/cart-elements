911b992e0d3d0641e59c62b392c8d4cd38f7a916
Shihab Uddin <shihabuddin.dev@gmail.com>
2025-02-23 23:51:36 +0600

    added new elements anyone can add here anythings

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-console.log('Shihab');
+const btnAdd = document.getElementById('btn-add')
+    .addEventListener('click', function () {
+        const inputName = document.getElementById('input-name').value;
+        const inputImg = document.getElementById('input-img').value;
+        const inputDesc = document.getElementById('input-desc').value;
+        
+        
+        const productList = document.getElementById('product-list');
+        
+        const div = document.createElement("div");
+        div.innerHTML = `
+        <h2>${inputName}</h2>
+        <img src=${inputImg} alt="">
+        <p>${inputDesc}</p>
+        `;
+        productList.appendChild(div);
+    })
+
